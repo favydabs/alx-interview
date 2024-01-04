18ee019807f6a90fbb4c99bf122b0d8bccba28d3
favydabs <favourdabit@gmail.com>
2024-01-04 08:17:23 +0100

    utf-8

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -3,15 +3,15 @@
 
 
 def validUTF8(data):
-    """ tracking the number of consecutive 1s in the most significant bit"""
+    # tracking the number of consecutive 1s in the most significant bit
     num_bytes = 0
 
     for byte in data:
-        """ Check if the most significant bit is 0 for a single-byte character"""
+        # Check if the most significant bit is 0 for a single-byte character
         if num_bytes == 0:
             if (byte >> 7) == 0:
                 continue
-             # Checking for consecutive 1s in the most significant bit
+        # Determine the number of consecutive 1s in the most significant bit
             elif (byte >> 5) == 0b110:
                 num_bytes = 1
             elif (byte >> 4) == 0b1110:
@@ -21,10 +21,10 @@ def validUTF8(data):
             else:
                 return False
         else:
-             #Check if the byte is a continuation byte
+            # Check if the byte is a continuation byte
             if (byte >> 6) != 0b10:
                 return False
             num_bytes -= 1
 
-    """Check if there are any incomplete multi-byte characters"""
+    # Check if there are any incomplete multi-byte characters
     return num_bytes == 0
